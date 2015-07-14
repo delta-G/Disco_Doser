d31fbe9bbe432dc71d92d7a619e5db666745b9cb
David <disco47dave@gmail.com>
2015-07-13 21:47:34 -0500

    started changes to DosingPump class

--- DosingPump.h
+++ DosingPump.h
@@ -13,23 +13,18 @@
 class DosingPump{
   
         public: 
-        unsigned int minimum_flow_rate;
-        unsigned int maximum_flow_rate;
-        int minimum_pwm_rate;
-        int maximum_pwm_rate;
+        unsigned int flow_rate;
+        int pwm_rate;
         byte pump_pin;
         
         DosingPump(byte pump_pin);
 	    DosingPump();
         void initPump(byte);
-        void runDosingPump(unsigned int volume, int rate);
-        unsigned long startDosingPump(unsigned int volume, int rate);
+
         unsigned long startDosingPump(unsigned int volume);
         void pumpOn();
         void pumpOff();
         
-        private:
-        int rateToPWM(int rate);
         
 };
 
